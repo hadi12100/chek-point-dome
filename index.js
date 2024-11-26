ad53d7698c0aeb111b97dc1c77b615d6561a5b96
hadi12100 <barkallahhadi579@gmail.com>
2024-11-26 09:43:54 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var container = document.getElementById("container")
+var box = document.getElementsByClassName("box")
+var total-price = document.getElementById("total-cost")
+
+for(i=0;i< boxs length;i++){
+    let box=boxs[i]
+}
+var minus = box.getElementsByClassName(" minus")
+console.log(minus)
+var plus =  box.getElementsByClassName(" plus")
+console.log(plus)
+var quantity = box.getElementsByClassName("quantity")
+console.log(quantity)
+var btndelete = box.getElementsByClassName("btndelete")
+console.log(btndelete)
+    
